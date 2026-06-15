ddcd8d3dfcbcf662763509a8dac26c5d788116c6
kucas2511-prog <kucas2511@gmail.com>
2026-06-15 10:39:43 -0300

    Versão 6
    Inclusão campo de exclusão em lote, mais função de aviso para nova versão.

---
+++ sw.js
@@ -0,0 +1,29 @@
+const V = 'saldo-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(V).then(c => c.add('index.html')).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== V).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(V).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+  }
+});
